5a66984eddcc47759a431286c21ecb5a36a3d929
Serhii Hrudakov <serhii@digitalchefs.nl>
2018-10-22 21:19:54 +0200

    Updated position reducer

--- src/views/SettingsView/reducers/toolbarPosition.js
+++ src/views/SettingsView/reducers/toolbarPosition.js
@@ -5,7 +5,14 @@ import { TOOL_BAR_POSITION } from './../../../constants';
 
 const toolbarPosition = (state = TOOL_BAR_POSITION.UP, action) => {
   switch (action.type) {
-    case TOOL_BAR_POSITION.DOWN: return TOOL_BAR_POSITION.DOWN;
+    case TOOL_BAR_POSITION.DOWN:
+      return TOOL_BAR_POSITION.DOWN;
+    case TOOL_BAR_POSITION.UP:
+      return TOOL_BAR_POSITION.UP;
+    case TOOL_BAR_POSITION.LEFT:
+      return TOOL_BAR_POSITION.LEFT;
+    case TOOL_BAR_POSITION.RIGHT:
+      return TOOL_BAR_POSITION.RIGHT;
     default:
       return state;
   }
